825bdac917a3edef7d665b8781902ae012ab64ac
winoros <winoros@gmail.com>
2015-01-27 12:00:40 +0800

    dominator tree
    o(mlog(m)) achieved
    trying to achieve o(mα(m,n))

--- Hdu/4694.cpp
+++ Hdu/4694.cpp
@@ -37,7 +37,7 @@
 
 using namespace std;
 
-vector<int> succ[50010], prod[50010], bucket[50010];
+vector<int> succ[50010], prod[50010], bucket[50010], dom_t[50010];
 int semi[50010], anc[50010], idom[50010], best[50010], fa[50010];
 int dfn[50010], redfn[50010];
 int timestamp;
@@ -45,12 +45,14 @@ int timestamp;
 void dfs(int now) {
 	dfn[now] = ++timestamp;
 	redfn[timestamp] = now;
-	anc[now] = idom[now] = 0;
+	anc[timestamp] = idom[timestamp] = 0;
 	semi[timestamp] = best[timestamp] = timestamp;
 	int sz = succ[now].size();
 	for(int i = 0; i < sz; ++i) {
-		if(fa[succ[now][i]] == -1)
-			dfs(succ[now][i]), fa[succ[now][i]] = now;
+		if(dfn[succ[now][i]] == -1) {
+			dfs(succ[now][i]);
+			fa[dfn[succ[now][i]]] = dfn[now];
+		}
 		prod[dfn[succ[now][i]]].push_back(dfn[now]);
 	}
 }
@@ -74,12 +76,16 @@ int eval(int now) {
 void lengauer_tarjan(int n) {
 	memset(dfn, -1, sizeof dfn);
 	memset(fa, -1, sizeof fa);
-	timestamp = -1;
+//	memset(anc, 0, sizeof anc);
+//	memset(idom, 0, sizeof idom);
+//	for(int i = 0; i <= n; ++i)
+//		best[i] = semi[i] = i;
+	timestamp = 0;
 	dfs(n);
-	for(int w = n-1; w > 0; --w) {
+	fa[1] = 0;
+	for(int w = timestamp; w > 1; --w) {
 		if(fa[w] == -1)
 			continue;
-		printf("%d___\n", w);
 		int sz = prod[w].size();
 		for(int i = 0; i < sz; ++i) {
 			int u = eval(prod[w][i]);
@@ -88,6 +94,8 @@ void lengauer_tarjan(int n) {
 		}
 		bucket[semi[w]].push_back(w);
 		anc[w] = fa[w];
+		if(fa[w] == 0)
+			continue;
 		sz = bucket[fa[w]].size();
 		for(int i = 0; i < sz; ++i) {
 			int u = eval(bucket[fa[w]][i]);
@@ -98,44 +106,41 @@ void lengauer_tarjan(int n) {
 		}
 		bucket[fa[w]].clear();
 	}
-	for(int i = 0; i < n; ++i)
-		printf("%d ", semi[i]);
-	puts("");
-	for(int i = 0; i < n; ++i)
-		printf("%d ", idom[i]);
-	puts("");
-	for(int w = 1; w < n; ++w) {
+	for(int w = 2; w <= n; ++w) {
 		if(fa[w] == -1)
 			continue;
 		if(idom[w] != semi[w])
 			idom[w] = idom[idom[w]];
 	}
-	idom[0] = 0;
+	idom[1] = 0;
+	for(int i = timestamp; i > 1; --i) {
+		if(fa[i] == -1)
+			continue;
+		dom_t[idom[i]].push_back(i);
+	}
 }
 
 long long ans[50010];
 
 void get_ans(int now) {
 	ans[redfn[now]] += redfn[now];
-	int sz = succ[redfn[now]].size();
+	int sz = dom_t[now].size();
 	for(int i = 0; i < sz; ++i) {
-		ans[succ[redfn[now]][i]] += ans[redfn[now]];
-		get_ans(succ[redfn[now]][i]);
+		ans[redfn[dom_t[now][i]]] += ans[redfn[now]];
+		get_ans(dom_t[now][i]);
 	}
 }
 
 void MAIN(int n, int m) {
 	for(int i = 0; i <= n; ++i)
-		succ[i].clear(), prod[i].clear(), bucket[i].clear();
+		succ[i].clear(), prod[i].clear(), bucket[i].clear(), dom_t[i].clear();
 	for(int i = 0, u, v; i < m; ++i) {
 		scanf("%d%d", &u, &v);
 		succ[u].push_back(v);
 	}
 	lengauer_tarjan(n);
-	for(int i = 0; i < n; ++i)
-		printf("%d%c", idom[i], i == n-1 ? '\n' : ' ');
 	memset(ans, 0, sizeof ans);
-	get_ans(0);
+	get_ans(1);
 	for(int i = 1; i <= n; ++i)
 		printf("%I64d%c", ans[i], i == n ? '\n' :' ');
 }
