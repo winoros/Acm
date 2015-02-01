b761c44276b079a1a9002b3b9bac6868ecf5a57f
winoros <winoros@gmail.com>
2015-02-01 23:45:48 +0800

    inclusion-exclusion principal

--- LightOj/1095.cpp
+++ LightOj/1095.cpp
@@ -21,32 +21,32 @@ using namespace std;
 
 const int mod = 1e9+7;
 
-long long a[1010], c[1010][1010];
+int a[1010], c[1010][1010];
 
 void init() {
-	a[0] = 1;
+	a[0] = c[0][0] = 1;
 	for(int i = 1; i <= 1000; ++i)
-		a[i] = a[i-1] * i %mod;
+		a[i] = (long long)a[i-1] * i %mod;
 	c[1][1] = c[1][0] = 1;
 	for(int i = 2; i <= 1000; ++i) {
 		c[i][0] = c[i][i] = 1;
 		for(int j = 1; j < i; ++j)
-			c[i][j] = (c[i-1][j-1] + c[i-1][j]) %mod;
+			c[i][j] = (long long)(c[i-1][j-1] + c[i-1][j]) %mod;
 	}
 }
 
 void MAIN() {
 	int n, m, k;
 	scanf("%d%d%d", &n, &m, &k);
-	long long ans = 0;
+	int ans = 0;
 	for(int i = 0; i <= m-k; ++i) {
 		if(i&1)
-			ans = ((ans - c[m-k][i]*a[n-k-i] %mod) %mod + mod) %mod;
+			ans = ((ans - (long long)c[m-k][i]*a[n-k-i] %mod) %mod + mod) %mod;
 		else
-			ans = (ans + c[m-k][i]*a[n-k-i] %mod) %mod;
+			ans = (ans + (long long)c[m-k][i]*a[n-k-i] %mod) %mod;
 	}
-	ans = ans * c[m][k] %mod;
-	printf("%lld\n", ans);
+	ans = (long long)ans * c[m][k] %mod;
+	printf("%d\n", ans);
 }
 
 int main() {
